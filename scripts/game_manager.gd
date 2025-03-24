68f627008693f12f5dc218083b43b79b0b03b78c
Jan Nothacker <git@jan-nothacker.de>
2025-03-24 21:09:12 +0100

    add book i guess

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,7 @@
 extends Node
 
 var quit_queued: bool = false
+var settings_queued: bool = false
 var anim_player: AnimationPlayer
 
 func _ready() -> void:
@@ -10,8 +11,15 @@ func _ready() -> void:
 		push_error("Anim Tree reference isn't set!")
 
 func request_quit():
-	print("quit requested")
 	if quit_queued:
 		return
 	quit_queued = true
 	anim_player.play(&"Quit")
+
+func request_settings():
+	if quit_queued:
+		print("quit already qued")
+		return
+	if settings_queued:
+		return
+	# TODO anim player
