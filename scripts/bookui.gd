e62d31c7029f9a39388c5e2f6da380ac423a8be7
Jan Nothacker <git@jan-nothacker.de>
2025-03-25 00:44:51 +0100

    ui transistion into settings

--- scripts/bookui.gd
+++ scripts/bookui.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 @onready var settings_btn: Button = $Control/MarginContainer/SettingsBtn
+@onready var settings_ui: Control = $Control/MarginContainer/SettingsUI
+
+func _ready() -> void:
+	settings_btn.visible = true
+	settings_ui.visible = false
 
 func _on_settings_btn_pressed() -> void:
 	if GameManager.request_settings():
 		settings_btn.visible = false
+		settings_ui.visible = true
