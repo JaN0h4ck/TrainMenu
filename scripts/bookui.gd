2fecd5eca830ed85c0f3ad31163f775134fe9226
Jan Nothacker <git@jan-nothacker.de>
2025-03-25 01:26:23 +0100

    back button functionality

--- scripts/bookui.gd
+++ scripts/bookui.gd
@@ -11,3 +11,11 @@ func _on_settings_btn_pressed() -> void:
 	if GameManager.request_settings():
 		settings_btn.visible = false
 		settings_ui.visible = true
+
+
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	GameManager.request_settings_to_idle()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,7 +8,7 @@ var anim_player: AnimationPlayer
 var config = ConfigFile.new()
 const SETTINGS_PATH: String = "user://settings.cfg"
 
-enum states {idling, quitting, settings}
+enum states {idling, quitting, settings, reverse_setting}
 
 var current_state: states = states.idling
 
@@ -31,7 +31,11 @@ func on_animation_finish(anim_name: StringName):
 	if(anim_name == &"Settings"):
 		current_state = states.settings
 		state_changed.emit(current_state)
-		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if(anim_name == &"ReverseSettings"):
+		current_state = states.idling
+		state_changed.emit(current_state)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func request_quit():
 	if quit_queued:
@@ -50,3 +54,10 @@ func request_settings() -> bool:
 	anim_player.play(&"Settings")
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	return true
+
+func request_settings_to_idle():
+	if current_state == states.settings:
+		current_state = states.reverse_setting
+		state_changed.emit(current_state)
+		anim_player.play(&"ReverseSettings")
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
