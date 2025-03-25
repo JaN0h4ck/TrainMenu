e05756ad310d6f2f5f9ab3a6a6becc320d347a6f
Jan Nothacker <git@jan-nothacker.de>
2025-03-25 17:43:23 +0100

    add driving animation

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+func _ready() -> void:
+	$TrainAnimationPlayer.play("TrainAnimation/default")
+
 func on_quit_animation_finish(anim_name: StringName):
 	if(anim_name == &"Quit"):
 		get_tree().quit()
