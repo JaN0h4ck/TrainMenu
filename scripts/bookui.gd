108a072ed49abac03f6c92d75d19d7a9c3af4f5f
Jan Nothacker <git@jan-nothacker.de>
2025-03-25 01:50:41 +0100

    lots of stuff

--- scripts/bookui.gd
+++ scripts/bookui.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @onready var settings_btn: Button = $Control/MarginContainer/SettingsBtn
 @onready var settings_ui: Control = $Control/MarginContainer/SettingsUI
+@onready var borderless: CheckBox = $Control/MarginContainer/SettingsUI/HBoxContainer/Left/Borderless
 
 func _ready() -> void:
 	settings_btn.visible = true
 	settings_ui.visible = false
+	borderless.disabled = true
 
 func _on_settings_btn_pressed() -> void:
 	if GameManager.request_settings():
@@ -14,8 +16,18 @@ func _on_settings_btn_pressed() -> void:
 
 
 func _on_full_screen_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
+	if toggled_on:
+		borderless.disabled = false
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		return
+	borderless.disabled = true
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
+func _on_borderless_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		return
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
 
 func _on_back_button_pressed() -> void:
 	GameManager.request_settings_to_idle()
