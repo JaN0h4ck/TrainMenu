f500d9502cfe957c430c8f2fd326b52feb4325b3
Jan Nothacker <git@jan-nothacker.de>
2025-03-26 15:08:13 +0100

    add ambient audio

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 func _ready() -> void:
 	$TrainAnimationPlayer.play("TrainAnimation/default")
+	$AmbientPlayer.play()
 
 func on_quit_animation_finish(anim_name: StringName):
 	if(anim_name == &"Quit"):
